d6f797cf659d30fd56b8d4964a01525cbdef6f68
JulianVel11 <julianvelandia18@gmail.com>
2024-07-15 10:13:26 -0500

    Creacion de layout edicion de perfil de repartidor + anadido de variables a testUser.js + anadido de estilo al header

---
+++ src/composables/testOrders.js
@@ -0,0 +1,36 @@
+import { ref } from 'vue'
+
+// Order Information
+export const orders = ref([
+    {
+        order_id:1,
+        restaurant_image:"/src/assets/images/image-profile-user.jpg",
+        order_address:"San Juanito",
+        order_restaurant_name:"Al Wok",
+    },
+    {
+        order_id:1,
+        restaurant_image:"/src/assets/images/image-profile-user.jpg",
+        order_address:"Algarra",
+        order_restaurant_name:"KFC",
+    },
+    {
+        order_id:1,
+        restaurant_image:"/src/assets/images/image-profile-user.jpg",
+        order_address:"Algarra",
+        order_restaurant_name:"KFC",
+    },
+    {
+        order_id:1,
+        restaurant_image:"/src/assets/images/image-profile-user.jpg",
+        order_address:"Algarra",
+        order_restaurant_name:"KFC",
+    },
+    {
+        order_id:1,
+        restaurant_image:"/src/assets/images/image-profile-user.jpg",
+        order_address:"Algarra",
+        order_restaurant_name:"KFC",
+    }
+
+])
--- src/composables/testUser.js
+++ src/composables/testUser.js
@@ -5,7 +5,12 @@ export const users = ref([
     {
         user_id:1,
         user_image:"/src/assets/images/image-profile-user.jpg",
-        user_name:"Juan Martinez",
+        user_name:"Juan Camilo",
+        user_lastname:"Martinez Martinez",
+        user_number:3131313131,
+        user_email:"juanmartinez@voykon.com",
+        user_age:28,
+        user_description:"Hola soy Juan y me encanta trabajar para Voykon",
         user_status:1,
     }
 ])
